6561aa57a7a90851e6be1d254d992081077ac10f
Thirasara Dewmin <127840071+thimin@users.noreply.github.com>
2024-09-16 18:49:49 +0530

    All CRUD operations with image uploading/ updating / deleting
    When we're going to update the image at the same time image also update in the folder directory. Deletion part also same

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+
+import Clothes from './components/inventory-management/clothes';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+
+      {/* toast message */}
+      {/*<Toaster position="top-center" reverseOrder={false}/>*/}
+{/*
       
-    </div>
+      <div>
+        <Header toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen}/>
+        <div style={{ paddingTop: "0px", paddingLeft: isSidebarOpen ? "250px" : "0" }}>
+*/}
+
+        <Routes>
+
+
+          <Route path="/clothes" element={<Clothes/>}/>
+
+        </Routes>
+    </BrowserRouter>
   );
 }
 
